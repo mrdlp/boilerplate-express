1549e8a1b0e15d43b30db1d58a72338c781bcbef
miguel <78372528+mrdlp@users.noreply.github.com>
2022-06-14 14:58:49 +0000

    add middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 console.log(__dirname);
 console.log("Hello Worldi");
 
+let public_path = __dirname + "/public"
+app.use("/public", express.static(public_path))
 
 app.get("/", (req, res) => {
   let absolutePath = __dirname + "/views/index.html"
