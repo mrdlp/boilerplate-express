7d2d41da35fab0fe3b4beb975027e96178d9fc89
miguel <78372528+mrdlp@users.noreply.github.com>
2022-06-14 14:53:57 +0000

    send file response

--- myApp.js
+++ myApp.js
@@ -1,10 +1,12 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+console.log(__dirname);
+console.log("Hello Worldi");
 
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  let absolutePath = __dirname + "/views/index.html"
+  res.sendFile(absolutePath);
 });
 
 
